ab95346593409548b50b9646e1f2e1016ebf00eb
festusOp7 <70211809+festusOp7@users.noreply.github.com>
2022-07-13 22:46:28 +0200

    Modles

---
+++ Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApi.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; } = null!;
+        public string Department { get; set; } = null!;
+        public string DateOfJoining { get; set; } = null!;
+        public string PhotoFileName { get; set; } = null!;
+    }
+}
